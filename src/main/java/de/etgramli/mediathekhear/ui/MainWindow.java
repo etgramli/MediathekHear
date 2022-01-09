22d107af40a76bee570a51b5b64d433f1bec1c4a
Etienne Gramlich <etgramlich@gmx.de>
2022-01-09 13:11:55 +0100

    Tabs with tables, dummy menu bar

--- src/main/java/de/etgramli/mediathekhear/ui/MainWindow.java
+++ src/main/java/de/etgramli/mediathekhear/ui/MainWindow.java
@@ -1,5 +1,7 @@
 package de.etgramli.mediathekhear.ui;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
 import javax.swing.*;
+import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
 
@@ -17,17 +20,34 @@ public class MainWindow extends JFrame {
     private static final int DEFAULT_WIDTH = 1280;
     private static final int DEFAULT_HEIGHT = 720;
 
-    public MainWindow(@Autowired @Nonnull final EpisodeTableModel tableModel) {
+    public MainWindow(@Autowired @Nonnull final ProgramsetsTableModel programsetsTableModel,
+                      @Autowired @Nonnull final EpisodeTableModel episodeTableModel) {
+        // Frame settings
         setTitle("MediathekHear");
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        final JTable podcastTable = new JTable(tableModel);
-        podcastTable.setRowSorter(new TableRowSorter<>(podcastTable.getModel()));
+        // Menu bar
+        final JMenuBar menuBar = new JMenuBar();
+        final JMenuItem reloadData = new JMenuItem("Daten aktualisieren");
+        reloadData.addActionListener(e -> {throw new UnsupportedOperationException();});
+        menuBar.add(reloadData);
+        setJMenuBar(menuBar);
 
-        final JScrollPane scrollPane = new JScrollPane(podcastTable);
-        podcastTable.setFillsViewportHeight(true);
-        add(scrollPane);
+        // Main content
+        final JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Podcasts", getSortableTableInScrollPane(programsetsTableModel));
+        tabbedPane.addTab("Episoden", getSortableTableInScrollPane(episodeTableModel));
+        add(tabbedPane);
+    }
+
+    @Contract("_ -> new")
+    @NotNull
+    private static JScrollPane getSortableTableInScrollPane(@Nonnull final TableModel tableModel) {
+        final JTable table = new JTable(tableModel);
+        table.setRowSorter(new TableRowSorter<>(table.getModel()));
+        table.setFillsViewportHeight(true);
+        return new JScrollPane(table);
     }
 }
