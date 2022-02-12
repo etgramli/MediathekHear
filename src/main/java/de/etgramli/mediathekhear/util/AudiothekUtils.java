21fb58cd84a2d02c6c3d267fd5235ae9df282029
Etienne Gramlich <etgramlich@gmx.de>
2022-02-12 20:31:21 +0100

    No more caching in AudiothekUtils

--- src/main/java/de/etgramli/mediathekhear/util/AudiothekUtils.java
+++ src/main/java/de/etgramli/mediathekhear/util/AudiothekUtils.java
@@ -17,7 +17,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Methods for querying from the Ard Audiothek (REST) API.
@@ -26,9 +28,6 @@ public final class AudiothekUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(AudiothekUtils.class);
     public static final String PROGRAMSETS_BASE_URL = "https://api.ardaudiothek.de/programsets";
-    private static Set<MtProgramSet> PROGRAMSETS;
-    private static final Map<Long, List<MtItem>> EPISODES = new HashMap<>();
-
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
@@ -40,55 +39,50 @@ public final class AudiothekUtils {
     @Nonnull
     @Unmodifiable
     public static Set<MtProgramSet> getProgramsets() {
-        if (PROGRAMSETS == null) {
-            logger.info("Querying program sets.");
-            try {
-                final URL programsetsUrl = new URL(PROGRAMSETS_BASE_URL);
+        logger.info("Querying program sets.");
+        try {
+            final URL programsetsUrl = new URL(PROGRAMSETS_BASE_URL);
 
-                final Programsets programsets = MAPPER.readValue(programsetsUrl, Programsets.class);
+            final Programsets programsets = MAPPER.readValue(programsetsUrl, Programsets.class);
 
-                PROGRAMSETS = Set.copyOf(programsets.embedded().mtProgramSets());
-            } catch (MalformedURLException e) {
-                logger.error("Could not parse URL: ", e);
-            } catch (IOException e) {
-                logger.error("Could not parse JSON: ", e);
-            }
+            return Set.copyOf(programsets.embedded().mtProgramSets());
+        } catch (MalformedURLException e) {
+            logger.error("Could not parse URL: ", e);
+        } catch (IOException e) {
+            logger.error("Could not parse JSON: ", e);
         }
-        return PROGRAMSETS;
+        return Collections.emptySet();
     }
 
     private static final String URL_PATTERN_LIMIT = "%s/%d?limit=%d";
+
     @Nonnull
     @Unmodifiable
     public static List<MtItem> getEpisodes(final long id) {
-        if (!EPISODES.containsKey(id)) {
-            logger.info("Querying episodes for program set: " + id);
-            try {
-                final String urlWithParams = URL_PATTERN_LIMIT.formatted(PROGRAMSETS_BASE_URL, id, MAX_NUMBER_OF_ELEMENTS_TO_FETCH);
-                final URL programsetUrl = new URL(urlWithParams);
+        logger.info("Querying episodes for program set: " + id);
+        try {
+            final String urlWithParams = URL_PATTERN_LIMIT.formatted(PROGRAMSETS_BASE_URL, id, MAX_NUMBER_OF_ELEMENTS_TO_FETCH);
+            final URL programsetUrl = new URL(urlWithParams);
 
-                final Programset programset = MAPPER.readValue(programsetUrl, Programset.class);
+            final Programset programset = MAPPER.readValue(programsetUrl, Programset.class);
 
-                if (programset != null) {
-                    final Embedded embedded = programset.embedded();
-                    if (embedded != null) {
-                        List<MtItem> items = embedded.mtItems();
-                        if (items == null) {
-                            logger.warn(String.format("Programset with id %d contained no episodes!", id));
-                            items = Collections.emptyList();
-                        }
-                        EPISODES.put(id, List.copyOf(items));
+            if (programset != null) {
+                final Embedded embedded = programset.embedded();
+                if (embedded != null) {
+                    List<MtItem> items = embedded.mtItems();
+                    if (items == null) {
+                        logger.warn(String.format("Programset with id %d contained no episodes!", id));
+                        items = Collections.emptyList();
                     }
+                    return items;
                 }
-            } catch (MalformedURLException e) {
-                logger.error("Could not parse URL: ", e);
-                EPISODES.put(id, Collections.emptyList());
-            } catch (IOException e) {
-                logger.error("Could not parse JSON: ", e);
-                EPISODES.put(id, Collections.emptyList());
             }
+        } catch (MalformedURLException e) {
+            logger.error("Could not parse URL: ", e);
+        } catch (IOException e) {
+            logger.error("Could not parse JSON: ", e);
         }
-        return EPISODES.get(id);
+        return Collections.emptyList();
     }
 
     @NotNull
